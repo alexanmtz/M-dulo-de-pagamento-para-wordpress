36eb46b44bdb84479c5d3ab7d4c1f9ae1dfcb33d
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2010-06-13 20:22:50 -0300

    paginacao no admin

--- modulo-vendas.php
+++ modulo-vendas.php
@@ -1,107 +1,134 @@
 <?php
 
-	global $wpdb;
-	$table_name = $wpdb->prefix . "modulo_venda";
+require "pagination.class.php";
 
-	$obter_vendas = "SELECT * from " . $table_name;
-  	$vendas = $wpdb->get_results($obter_vendas);
+global $wpdb;
+$table_name = $wpdb->prefix . "modulo_venda";
 
-  	if($_GET['action']=='status') {
-	  	if($_GET['error']) {
-	  		$message = "Não foi possível modificar o status";
-	  	} else {
-	  		$message = "Status modificado com sucesso";
-	  	}
-  	}
+if($_GET['action']=='status') {
+	if($_GET['error']) {
+		$message = "Não foi possível modificar o status";
+	} else {
+		$message = "Status modificado com sucesso";
+	}
+}
 
-  	if($_GET['action']=='apagar') {
-  	  	if($_GET['error']) {
-	  		$message = "Não foi possível excluir a venda";
-	  	} else {
-	  		$message = "Vendas excluídas com sucesso";
-	  	}
-  	}
+if($_GET['action']=='apagar') {
+	if($_GET['error']) {
+		$message = "Não foi possível excluir a venda";
+	} else {
+		$message = "Vendas excluídas com sucesso";
+	}
+}
+ 
+$items = mysql_num_rows(mysql_query("SELECT * from $table_name")); // number of total rows in the database
+
+if($items > 0) {
+	$p = new pagination;
+	$p->items($items);
+	$p->limit(10); // Limit entries per page
+	$p->target("tools.php?page=M-dulo-de-pagamento-para-wordpress/modulo-vendas.php");
+	$p->currentPage($_GET[$p->paging]); // Gets and validates the current page
+	$p->calculate(); // Calculates what to show
+	$p->parameterName('paging');
+	$p->adjacents(1); //No. of page away from the current page
+
+	if(!isset($_GET['paging'])) {
+		$p->page = 1;
+	} else {
+		$p->page = $_GET['paging'];
+	}
+
+	//Query for limit paging
+	$limit = "LIMIT " . ($p->page - 1) * $p->limit  . ", " . $p->limit;
+	
+	$obter_vendas = "SELECT * from $table_name order by id desc $limit";
+	$vendas = $wpdb->get_results($obter_vendas);
+	
+
+} else {
+	echo "No Record Found";
+}
+ 
 
 ?>
 <?php if ( $message != false ) { ?>
-<div id="message" class="updated fade"><p><?php echo $message; ?></p></div>
+<div id="message" class="updated fade">
+<p><?php echo $message; ?></p>
+</div>
 <?php } ?>
 <div class="wrap">
-	<h2><?php _e('Gerenciar vendas'); ?></h2>
-	<form action="admin-post.php" method="post">
-		<?php wp_nonce_field('modulo_venda_transacao'); ?>
-		<input type="hidden" name="action" value="modulo_venda_transacao">
-		<div class="tablenav">
-			<div class="alignleft">
-			<input type="submit" value="Apagar" name="modulo_venda_transacao" class="button-secondary delete" />
-			<select name='modulo-venda-status' id='filtro' class='postform' >
-				<option value='pendente'>Pendente</option>
-				<option value="aguardando pagamento">Aguardando Pagamento</option>
-				<option value="enviando">Enviando</option>
-				<option value="finalizado">Finalizado</option>
-			</select>
-			<input type="submit" id="post-query-submit" value="Modificar Status" name="modulo_venda_transacao" class="button-secondary" />
-		</div>
-		</div>
-		<br class="clear">
-		<table class="widefat">
-			<thead>
-				<tr valign="top">
-					<th class="check-column" scope="col"></th>
-					<th scope="col"><?php _e('Data'); ?></th>
-					<th scope="col"><?php _e('Nome'); ?></th>
-					<th scope="col"><?php _e('Produtos'); ?></th>
-					<th scope="col"><?php _e('Valor'); ?></th>
-					<th scope="col"><?php _e('Modo de envio'); ?></th>
-					<th scope="col"><?php _e('E-mail'); ?></th>
-					<th scope="col"><?php _e('Status'); ?></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $modos_de_envio = array(
+<h2><?php _e('Gerenciar vendas'); ?></h2>
+<form action="admin-post.php" method="post"><?php wp_nonce_field('modulo_venda_transacao'); ?>
+<input type="hidden" name="action" value="modulo_venda_transacao">
+<div class="tablenav">
+<div class="tablenav-pages">
+<?php echo $p->show();  // Echo out the list of paging. ?>
+</div>
+<div class="alignleft"><input type="submit" value="Apagar"
+	name="modulo_venda_transacao" class="button-secondary delete" /> <select
+	name='modulo-venda-status' id='filtro' class='postform'>
+	<option value='pendente'>Pendente</option>
+	<option value="aguardando pagamento">Aguardando Pagamento</option>
+	<option value="enviando">Enviando</option>
+	<option value="finalizado">Finalizado</option>
+</select> <input type="submit" id="post-query-submit"
+	value="Modificar Status" name="modulo_venda_transacao"
+	class="button-secondary" /></div>
+</div>
+<br class="clear">
+<table class="widefat">
+	<thead>
+		<tr valign="top">
+			<th class="check-column" scope="col"></th>
+			<th scope="col"><?php _e('Data'); ?></th>
+			<th scope="col"><?php _e('Nome'); ?></th>
+			<th scope="col"><?php _e('Produtos'); ?></th>
+			<th scope="col"><?php _e('Valor'); ?></th>
+			<th scope="col"><?php _e('Modo de envio'); ?></th>
+			<th scope="col"><?php _e('E-mail'); ?></th>
+			<th scope="col"><?php _e('Status'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php $modos_de_envio = array(
 					'SD' => 'Sedex',
 					'EN' => 'Encomenda normal',
 					'gratis' => 'Grátis'
-				); ?>
-				<?php foreach( $vendas as $venda ) { ?>
-				<?php
+					); ?>
+					<?php foreach( $vendas as $venda ) { ?>
+					<?php
 					$produtos = $venda->produto_id;
 					$array_produtos = explode(',',$produtos);
-				?>
-				<tr>
-					<th scope="row" class="check-column <?php if ($count == 1){echo 'alternate';} ?>">
-					<input type="checkbox" valign="bottom" value="<?php echo $venda->id; ?>" name="vendas[]"/></th>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $venda->data; ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $venda->nome; ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php $produtos = modulo_venda_obter_produtos($array_produtos); ?>
-						<?php foreach($produtos as $i => $produto ) {
-						      echo $produtos[$i]['descricao'];
-							  echo ' - ';
-							  echo $produtos[$i]['quantidade']."itens - ";
-							  echo 'R$'.$produtos[$i]['valor']."<br/>";
-						 } ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $venda->valor; ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $modos_de_envio[$venda->envio]; ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $venda->email; ?>
-					</td>
-					<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
-						<?php echo $venda->status; ?>
-					</td>
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</form>
+					?>
+		<tr>
+			<th scope="row"
+				class="check-column <?php if ($count == 1){echo 'alternate';} ?>"><input
+				type="checkbox" valign="bottom" value="<?php echo $venda->id; ?>"
+				name="vendas[]" /></th>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $venda->data; ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $venda->nome; ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php $produtos = modulo_venda_obter_produtos($array_produtos); ?> <?php foreach($produtos as $i => $produto ) {
+				echo $produtos[$i]['descricao'];
+				echo ' - ';
+				echo $produtos[$i]['quantidade']."itens - ";
+				echo 'R$'.$produtos[$i]['valor']."<br/>";
+			} ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $venda->valor; ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $modos_de_envio[$venda->envio]; ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $venda->email; ?></td>
+			<td class="<?php if ($count == 1){echo 'alternate';} ?>" valign="top">
+			<?php echo $venda->status; ?></td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
+</form>
 
 </div>
